e686290a321badeeaf1decb34fb2e26c594d904a
Ivory <103183001+IvoryXL@users.noreply.github.com>
2022-07-10 18:57:32 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,11 @@
 if game.PlaceId == 606849621 then
     local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
     local Window = Library.CreateLib("IvorHub : Jailbreak", "DarkTheme")
-    local Tab = Window:NewTab("Player")
+    local PlayerTab = Window:NewTab("Player")
+    -- Create a section for the PlayerTab
+    local PlayerSection = PlayerTab:NewSection("Player")
+    -- Create a slider for the PlayerSection
+    local PlayerSlider = PlayerSection:NewSlider("Walk Speed", "Changes the player's walkspeed", 16, 100, function(value)
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = value
+    end)
 end
