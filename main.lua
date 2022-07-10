e255be43c889a122cea6f865029377b0e17fc4be
Ivory <103183001+IvoryXL@users.noreply.github.com>
2022-07-10 19:04:36 +0100

    Maybe fixed speed glitch

--- main.lua
+++ main.lua
@@ -5,10 +5,13 @@ if game.PlaceId == 606849621 then
     -- Create a section for the PlayerTab
     local PlayerSection = PlayerTab:NewSection("Player")
     -- Create a slider for the PlayerSection
-    PlayerSection:NewSlider("SliderText", "SliderInfo", 16, 1000, function(s)
-        game.Players.LocalPlayer.Character.Humanoid.Changed:connect(function()
+    PlayerSection:NewSlider("SliderText", "SliderInfo", 500, 0, function(s)
+        -- Create a while loop
+        while true do
+            -- Change the players speed to s
             game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
-        end)
+        end
+        
     end)
 
 end
