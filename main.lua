596b9f606cbc1d5d8c9e085ed4d3ec4447e1d5ea
Ivory <103183001+IvoryXL@users.noreply.github.com>
2022-07-10 18:51:06 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
 if game.PlaceId == 606849621 then
     local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
     local Window = Library.CreateLib("IvorHub : Jailbreak", "DarkTheme")
+    local Tab = Window:NewTab("LocalPlayer")
 end
