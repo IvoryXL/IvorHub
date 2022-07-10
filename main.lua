54066e83f62c8e3933d3fda58ad607b09b901495
Ivory <103183001+IvoryXL@users.noreply.github.com>
2022-07-10 19:13:08 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,15 +5,8 @@ if game.PlaceId == 606849621 then
     -- Create a section for the PlayerTab
     local PlayerSection = PlayerTab:NewSection("Player")
     -- Create a slider for the PlayerSection
-    PlayerSection:NewSlider("Walkspeed", "Change Walkspeed", 16, 250, function(s)
-        -- Create a while loop
-        while true do
-            -- Change the players speed to s
-            game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
-            -- Pause the script for 0.756 seconds
-            wait(2.5)
-        end
-        
+    PlayerSection:NewSlider("Walkspeed", "Change Walkspeed", 16, 50, function(s)
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
     end)
 
 end
